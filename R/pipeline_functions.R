43aba34bee69cfabf3e322df80673c7d31239745
wdwatkins <wwatkins@usgs.gov>
2021-01-11 16:24:02 -0800

    rerun with min/max for parameter values, new plots

--- R/pipeline_functions.R
+++ R/pipeline_functions.R
@@ -221,8 +221,100 @@ combine_summaries <- function(...) {
   file_names_split <- str_split(basename(unlist(summary_dots)), pattern = "_", simplify = TRUE)
   summary_tibble <- tibble(summary_file = c(summary_dots),
                            site_no = file_names_split[,2],
-                           parameter = file_names_split[,3]) %>% 
+                           parameter = gsub(pattern = ".rds", replacement = "",
+                                            x = file_names_split[,3])) %>% 
     rowwise() %>% 
     mutate(summary_object = list(readRDS(summary_file)))
   return(summary_tibble)
+}
+
+do_model_tasks <- function(joined_data_summary,
+                           split_method = 'test_future', 
+                           upper_bound_include,
+                           lower_bound_include,
+                           ...) {
+  task_names <- paste(joined_data_summary$site_no, joined_data_summary$parameter,
+                      sep = '_')
+  split_data_step <- create_task_step(
+    step_name = 'split_data',
+    target_name = function(task_name, ...) {
+      sprintf('3_model/out/%s_split_data.rds', task_name)
+    },
+    command = function(task_name, ...) {
+      joined_data_file <- paste0('2_process/out/joined_data_', task_name, ".rds")
+      
+      psprintf("create_train_test(outfile = target_name, joined_aq_adaps_file = '%s'," = joined_data_file, 
+               "split_method = I('test_future'), lower_bound_include = lower_bound,
+               upper_bound_include = upper_bound)")
+    }
+  )
+  
+  #model and predict step
+  model_log_reg_step <- create_task_step(
+    step_name = 'logistic_regression',
+    target_name = function(task_name, ...) {
+      sprintf('3_model/out/%s_log_reg_output.rds', task_name)
+    },
+    command = function(steps, ...) {
+      psprintf("model_and_predict_log_reg(outfile = target_name, 
+               split_data_file = '%s')" = steps[['split_data']]$target_name)
+    }
+  )
+  #eval model step ROC curve, maybe prec-recall curve too?
+  roc_curve_step <- create_task_step(
+    step_name = 'roc_curve',
+    target_name = function(task_name, ...) {
+      sprintf('3_model/out/%s_log_reg_roc_curve.png', task_name)
+    },
+    command = function(steps, ...) {
+      psprintf("log_reg_roc_curve(outfile = target_name, 
+               model_output_file = '%s')" = steps[['logistic_regression']]$target_name)
+    }
+  )
+  
+  eval_model_step <- create_task_step(
+    step_name = 'model_eval',
+    target_name = function(task_name, ...) {
+      sprintf('%s_model_evalution', task_name)
+    },
+    command = function(steps, ...) {
+      psprintf("eval_model(model_output_file = '%s')" = steps[['logistic_regression']]$target_name)
+    }
+  )
+  
+  output_timeseries_dygraph_step <- create_task_step(
+    step_name = 'output_timeseries_dygraph',
+    target_name = function(task_name, ...) {
+      sprintf('3_model/out/dygraph_%s_model_output.html', task_name)
+    },
+    command = function(steps, ...) {
+      psprintf("dygraph_model_output(outfile = target_name,", 
+               "model_output_file = '%s'," = steps[['logistic_regression']]$target_name,
+               "split_data_file = '%s'," = steps[['split_data']]$target_name,
+               "model_eval_df = `%s`)" = steps[['model_eval']]$target_name)
+    }
+  )
+  
+  
+  task_plan <- create_task_plan(
+    task_names = task_names,
+    task_steps = list(split_data_step, model_log_reg_step, 
+                      roc_curve_step, eval_model_step, output_timeseries_dygraph_step),
+    add_complete = FALSE
+  )
+  
+  create_task_makefile(
+    task_plan = task_plan,
+    makefile = '3_models.yml',
+    include = c('2_join_summarize_tasks.yml'),
+    sources = c(...),
+    packages = c('tidymodels', 'lubridate'),
+    finalize_funs = c('generate_combine_model_metrics'),
+    final_targets = c('model_metrics'),
+    as_promises = TRUE
+  )
+  
+  loop_tasks(task_plan = task_plan, task_makefile = '3_models.yml', num_tries = 1)
+  model_metrics <- remake::fetch('model_metrics_promise', remake_file = '3_models.yml')
+  #return(model_metrics)
 }

6aaa0ec81dd04009f0e5ba354273156ae773ec01
wdwatkins <wwatkins@usgs.gov>
2021-01-14 14:39:22 -0800

    f1 score plot changes

--- R/results_f1_fig.R
+++ R/results_f1_fig.R
@@ -1,7 +1,12 @@
 library(ggplot2)
 mi <- remake::fetch('model_inventory')
-ggplot(mi, aes(x = train_f1, y = test_f1)) + geom_point() + 
+ggplot(mi, aes(x = train_f1, y = test_f1, size = train_n_positives_train + n_positives_test)) + 
+  geom_point() +
+  #geom_label() +
   labs(x = 'Training set F1 score (1 = perfect)', y = 'Test set F1 score (1 = perfect)', 
-       title = 'Anomaly detection model\nperformance by site') + 
-  geom_abline(slope = 1, linetype = 2)
+       title = 'Temperature anomaly detection\nmodel performance by site',
+       size = 'total number of anomalies') + 
+  geom_abline(slope = 1, linetype = 2) +
+  scale_x_continuous(limits = c(0,1)) +
+  scale_y_continuous(limits = c(0,1)) 
 ggsave('train_test_f1.png', width = 4, height = 4)
